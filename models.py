4b21716cd9d792bc3789d060597a8720d8cd8b2b
Richard Yao <2501.owlsome@gmail.com>
2019-06-20 23:13:59 +0800

    add artical model and build system

--- models.py
+++ models.py
@@ -1,3 +1,73 @@
+import os
+import markdown
+from datetime import datetime, timedelta
+import logging
 from django.db import models
+from django.conf import settings
+from django.utils import timezone
 
-# Create your models here.
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+
+class artical_cache(models.Model):
+    file_name = models.CharField(max_length=256)
+    title = models.CharField(max_length=256)
+    pub_time = models.DateTimeField()
+    update_time = models.DateTimeField()
+    content = models.TextField()
+
+    last_build_time = None
+
+    def is_expired(self):
+        p = os.path.join(settings.ARTICAL_ROOT, self.file_name)
+        return datetime.fromtimestamp(os.path.getmtime(p)) > self.update_time
+
+    def update(self):
+        p = os.path.join(settings.ARTICAL_ROOT, self.file_name)
+        artical = artical_cache.parse_md(p)
+        self.update_time = datetime.fromtimestamp(os.path.getmtime(p))
+        self.title = artical['title']
+        self.content = artical['content']
+        self.save()
+
+    @staticmethod
+    def parse_md(md_file_path):
+        md_ext = ['extra', 'codehilite', 'meta']
+        with open(md_file_path) as md_file:
+            md_str = md_file.read()
+        md = markdown.Markdown(extensions=md_ext)
+        content = md.convert(md_str)
+        logger.debug(content)
+        title = md.Meta.get('title', ('████████████████████'))[0]
+        logger.debug(title)
+        return {'title': title, 'content': content}
+
+    @classmethod
+    def build(cls):
+        last = cls.last_build_time
+        now = timezone.now()
+        if last is None or last - timedelta(seconds=600) < now:
+            for art_name in os.listdir(settings.ARTICAL_ROOT):
+                art_path = os.path.join(settings.ARTICAL_ROOT, art_name)
+                logger.debug(f'"{art_path}" process')
+                if (not os.path.isfile(art_path)
+                        and not art_name.endwith('.md')):
+                    continue
+                try:
+                    art_cache = cls.objects.get(file_name=art_name)
+                    if art_cache.is_expired():
+                        art_cache.update()
+                        logger.error(f'"{art_name}" updated')
+                except cls.DoesNotExist:
+                    artical = artical_cache.parse_md(art_path)
+                    pub_time = datetime.fromtimestamp(
+                        os.path.getmtime(art_path))
+                    art_cache = cls(file_name=art_name,
+                                    title=artical['title'],
+                                    pub_time=pub_time,
+                                    update_time=pub_time,
+                                    content=artical['content'])
+                    logger.error(artical['content'])
+                    logger.error(f'"{art_name}" created')
+                    art_cache.save()
--- views.py
+++ views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import artical_cache
 
-# Create your views here.
+
+class index(generic.ListView):
+    template_name = 'blog/index.html'
+    context_object_name = 'latest_artical_list'
+
+    def get_queryset(self):
+        artical_cache.build()
+        return artical_cache.objects.all()
