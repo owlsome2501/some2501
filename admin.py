20a247f13f3c20e619f39c1895017c6cb68c606e
Richard Yao <2501.owlsome@gmail.com>
2019-07-02 23:37:51 +0800

    temp refactor step 3: refactor build system

--- admin.py
+++ admin.py
@@ -1,4 +1,4 @@
 from django.contrib import admin
-from .models import artical
+from .models import artical, author
 
-admin.site.register(artical)
+admin.site.register(artical, author)
--- management/commands/build_cache.py
+++ management/commands/build_cache.py
@@ -1,34 +1,28 @@
 from os import listdir, path
-from datetime import datetime
 import logging
 from django.conf import settings
-from blog.models import artical, author, parse_md
+from blog.models import artical, author
 from django.core.management.base import BaseCommand
 
 logger = logging.getLogger('django')
 
 
 class Command(BaseCommand):
-    def build_author(self):
-        for author_name in listdir(settings.ARTICAL_ROOT):
-            author_home = path.join(settings.ARTICAL_ROOT, author_name)
-            author_self = path.join(author_home, author_name + 'md')
-            logger.debug(f'"{author_home}" process')
-            if path.isfile(author_self):
-                meta, description = parse_md(author_self)
-                nickname = meta.get('nickname', (author_name, ))[0]
-                mail = meta.get('mail', (None, ))[0]
-                new_author = author(name=author_name,
-                                    nickname=nickname,
-                                    mail=mail,
-                                    description=description)
-                logger.debug(description)
-                logger.info(f'"{author_name}" created')
-                new_author.save()
-
-    def build_art(self):
+    def build(self):
         logger.info('start build')
         for author_name in listdir(settings.ARTICAL_ROOT):
+            try:
+                au = author.object.get(name=author_name)
+                if au.is_expired():
+                    au.update()
+                    logger.info(f'"{au.name}" updated')
+            except author.DoesNotExist:
+                au = author.mk_author(author_name)
+                if au is not None:
+                    au.save()
+                    logger.info(f'"{au.name}" created')
+                else:
+                    continue
             author_home = path.join(settings.ARTICAL_ROOT, author_name)
             for art_name in listdir(author_home):
                 art_path = path.join(author_home, art_name)
@@ -36,22 +30,14 @@ class Command(BaseCommand):
                 if (not path.isfile(art_path) or not art_name.endswith('.md')):
                     continue
                 try:
-                    art_cache = artical.objects.get(file_name=art_name)
-                    if art_cache.is_expired():
-                        art_cache.update()
+                    art = artical.objects.get(author=au, file_name=art_name)
+                    if art.is_expired():
+                        art.update()
                         logger.info(f'"{art_name}" updated')
                 except artical.DoesNotExist:
-                    art = parse_md(art_path)
-                    pub_time = datetime.fromtimestamp(path.getmtime(art_path))
-                    art_cache = artical(file_name=art_name,
-                                              title=art['title'],
-                                              pub_time=pub_time,
-                                              update_time=pub_time,
-                                              content=art['content'])
-                    logger.debug(art['content'])
+                    art = artical.mk_artical(au, art_name)
                     logger.info(f'"{art_name}" created')
-                    art_cache.save()
+                    art.save()
 
     def handle(self, *args, **options):
-        self.build_author()
-        self.build_art()
+        self.build()
