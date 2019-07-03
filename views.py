1dc9dec922f5569e231dbfccb6035a0e2e142fbe
Richard Yao <2501.owlsome@gmail.com>
2019-07-03 14:52:49 +0800

    add author home page templates

--- views.py
+++ views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from .models import artical
+from .models import artical, author
 
 
 def index(request):
@@ -9,7 +9,19 @@ def index(request):
 
 
 def author_detail(request, author_name):
-    pass
+    au = get_object_or_404(author, name=author_name)
+    articals = artical.objects.filter(author=au)
+    name = au.nickname if au.nickname is not None else au.name
+    contex = {
+        'name': name,
+        'info_list': [{
+            'caption': 'Contact me',
+            'detail': au.mail
+        }],
+        'description': au.description.content,
+        'articals': articals
+    }
+    return render(request, 'blog/author.html', contex)
 
 
 def artical_detail(request, author_name, file_name):
